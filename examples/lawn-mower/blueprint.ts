bf168b4b0cced03df1283eac1a41e03da6ee62b9
mtrifonov <mtrifonov.design@gmail.com>
2026-01-05 22:53:44 +0100

    lawnmower game v0 is done

--- examples/lawn-mower/blueprint.ts
+++ examples/lawn-mower/blueprint.ts
@@ -5,6 +5,7 @@ import Quad from "../utils/quad";
 
 function lawnMowerScene(input: {
     lawnMowerInputImage: any,
+    lawnInputImage: any,
     lawnMowerPosition: { x: number, y: number },
     lawnMowerOrientation: number,
 }
@@ -19,7 +20,7 @@ function lawnMowerScene(input: {
         ],
         []
     )
-    const lawnMowerInputTexture = Texture({
+    const lawnInputTexture = Texture({
         width: 512,
         height: 512,
     },
@@ -35,33 +36,53 @@ function lawnMowerScene(input: {
                     data.push(0, 0, 0, 255);
                 }
             }
-            return input.lawnMowerInputImage;
+            return input.lawnInputImage;
         },
         [input.lawnMowerPosition.x, input.lawnMowerPosition.y, input.lawnMowerOrientation]);
 
+    const lawnMowerInputTexture = Texture({
+        width: 512,
+        height: 512,
+    },
+    () => {
+        return input.lawnMowerInputImage;
+    },
+    []);
 
     const grasshalmInstances = Instances({
         attributes: {
             i_position: 'vec2',
             i_angle: 'float',
             i_shadeVariation: 'float',
+            i_heightVariation: 'float',
         },
-        maxInstanceCount: 100000,
+        maxInstanceCount: 1000000,
     },
         {
-            count: 100000,
+            count: 1000000,
             instances: () => {
                 const output = [];
-                for (let i = 0; i < 100000; i++) {
+                for (let i = 0; i < 1000000; i++) {
                     output.push({
                         i_position: [Math.random() - 0.5, Math.random() - 0.5],
                         i_angle: Math.random() - 0.5,
-                        i_shadeVariation: Math.random() 
+                        i_shadeVariation: Math.random(),
+                        i_heightVariation: Math.random(),
                     });
                 }
                 return output;
             }
         }, []);
+
+    const uniforms = Uniforms({
+            lawnMowerPosition: 'vec2',
+            lawnMowerOrientation: 'float',
+    },  () => ({
+        lawnMowerPosition: [input.lawnMowerPosition.x, input.lawnMowerPosition.y],
+        lawnMowerOrientation: [input.lawnMowerOrientation],
+    }), [
+        input.lawnMowerPosition.x, input.lawnMowerPosition.y, input.lawnMowerOrientation
+    ]);
 
     const outputTexture = Texture({
         width: 1080,
@@ -76,9 +97,10 @@ function lawnMowerScene(input: {
             void main() {
                 uv = vec2(u, v);
                 v_shortGrass = texture(src, i_position + vec2(0.5)).r > 0.5 ? 1 : 0;
-                float angle = i_angle * 3.14159 * (v_shortGrass == 1 ? 0.25 : .1);
+                float angle = i_angle * 3.14159 * (v_shortGrass == 1 ? 0.25 : .15);
                 mat2 rotation = mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
-                float height = v_shortGrass == 1 ? 0.1 : 0.05;
+                float height = v_shortGrass == 1 ? 0.09 : 0.05;
+                height = height * (0.3 + i_heightVariation * 0.7);
                 vec2 finalPos = i_position + vec2(0.003, height) * uv * rotation;
                 gl_Position = vec4(finalPos,i_position.y, 1.0);
                 v_shadeVariation = i_shadeVariation;
@@ -90,7 +112,51 @@ function lawnMowerScene(input: {
             flat in int v_shortGrass;
             void main() {
                 outColor = vec4((0.0) + v_shadeVariation * 0.3, 
-                1.0 - v_shadeVariation * (v_shortGrass == 0 ? 0.2 : 0.4), 0.0, 1.0);
+                1.0 - v_shadeVariation * (v_shortGrass == 0 ? 0.3 : 0.8), 0.0, 1.0);
+            }
+            `,
+            {
+                textures: {
+                    src: {
+                        texture: lawnInputTexture,
+                        sampler: {
+                            wrap: 'clamp',
+                            filter: 'nearest',
+                        }
+                    },
+                },
+                instances: grasshalmInstances,
+            },
+            {
+            depthTest: true,
+            //blendMode: "alpha",
+            }
+
+        ),
+        DrawOp(
+            quad,
+            () => `
+            out vec2 uv;
+            void main() {
+                uv = vec2(u, v);
+                float angle = lawnMowerOrientation - 3.14159 / 2.0;
+                mat2 rotation = mat2(
+                    cos(angle), -sin(angle),
+                    sin(angle), cos(angle)
+                );
+                float scale = 0.1;
+                float aspect = 1.5;
+                vec2 finalPos = 
+                    ((position.xy + vec2(0.,-0.5)) * vec2(1.,aspect) * rotation) * scale
+                    + lawnMowerPosition + vec2(0.0,0.075);
+                gl_Position = vec4(finalPos, 0., 1.0);
+            }
+            `,
+            () => `
+            in vec2 uv;
+            void main() {
+            vec4 lawnMowerColor = texture(src, uv);
+            outColor = lawnMowerColor;
             }
             `,
             {
@@ -103,10 +169,14 @@ function lawnMowerScene(input: {
                         }
                     },
                 },
-                instances: grasshalmInstances,
+                uniforms: {
+                    uni: uniforms,
+                },
+            },
+            {
+            blendMode: "alpha",
             }
-
-        ),
+        )
         // DrawOp(
         //     quad,
         //     () => `
@@ -139,9 +209,11 @@ function lawnMowerScene(input: {
 
     return {
         quad,
+        uniforms,
         grasshalmInstances,
         outputTexture,
-        lawnMowerInputTexture
+        lawnMowerInputTexture,
+        lawnInputTexture,
     };
 
 
--- examples/lawn-mower/main.ts
+++ examples/lawn-mower/main.ts
@@ -5,6 +5,18 @@ import blueprint from './blueprint.ts'
 import { Drawing, GPUBackend, Blueprint } from "pinsandcurves-engine";
 import type { VirtualResourceGraph } from "pinsandcurves-engine";
 
+
+const lawnMowerCanvas = document.createElement('canvas');
+lawnMowerCanvas.width = 512;
+lawnMowerCanvas.height = 512;
+const lawnMowerCtx = lawnMowerCanvas.getContext('2d')!;
+const lawnImg = new Image();
+lawnImg.src = '/pinsandcurves-engine/examples/lawn-mower/lawn-mower.png';
+await new Promise((resolve) => {
+  lawnImg.onload = () => resolve(true);
+});
+lawnMowerCtx.drawImage(lawnImg, 0, 0, 512, 512);
+
 console.log("Lawn Mower example");
 
 // Asset Store is a mock class simulating a database or state management system that we can subscribe to for changes.
@@ -29,6 +41,7 @@ const unsubscribe = assetStore.subscribe((store, graphId) => {
 
 type WorldState = {
   lawnMowerInputImage: any,
+  lawnInputImage: any,
   lawnMowerPosition: { x: number, y: number },
   lawnMowerOrientation: number,
 }
@@ -38,78 +51,125 @@ type WorldState = {
 const gfx = new Blueprint();
 
 function updateDrawing(inputValue: WorldState) {
+  //const updatedBlueprint = blueprint(inputValue);
+  //console.log("Updating drawing with new blueprint:", updatedBlueprint);
   const { addedAssets, deletedAssetIds, graphId } = gfx.update(blueprint(inputValue));
   drawGraph(addedAssets[graphId] as VirtualResourceGraph); // updating the mermaid flow diagram
   assetStore.transaction(addedAssets, deletedAssetIds, graphId);
 }
 
 
-const lawnMowerCanvas = document.createElement('canvas');
-lawnMowerCanvas.width = 512;
-lawnMowerCanvas.height = 512;
-const lawnMowerCtx = lawnMowerCanvas.getContext('2d')!;
-lawnMowerCtx.fillStyle = 'white';
-lawnMowerCtx.fillRect(0, 0, 512, 512);
+const lawnCanvas = document.createElement('canvas');
+lawnCanvas.width = 512;
+lawnCanvas.height = 512;
+const lawnCtx = lawnCanvas.getContext('2d')!;
+lawnCtx.fillStyle = 'white';
+lawnCtx.fillRect(0, 0, 512, 512);
 const worldState: WorldState = {
+  lawnInputImage: lawnCanvas,
   lawnMowerInputImage: lawnMowerCanvas,
   lawnMowerPosition: { x: 0, y: 0 },
   lawnMowerOrientation: 0,
 };
 
+const lawnSampleCanvas = document.createElement('canvas');
+lawnSampleCanvas.width = 16;
+lawnSampleCanvas.height = 16;
+const lawnSampleCtx = lawnSampleCanvas.getContext('2d')!;
+
+const startTime = Date.now();
+
+const keysDown = new Set<string>();
 document.addEventListener('keydown', (event) => {
-  event.preventDefault();
-  const step = 0.02;
-  const rotStep = 0.1;
+  if (event.key === "ArrowUp" || event.key === "ArrowDown" || event.key === "ArrowLeft" || event.key === "ArrowRight") event.preventDefault();
+  keysDown.add(event.key);
+});
+document.addEventListener('keyup', (event) => {
+  if (event.key === "ArrowUp" || event.key === "ArrowDown" || event.key === "ArrowLeft" || event.key === "ArrowRight") event.preventDefault();
+  keysDown.delete(event.key);
+});
+
+let speed = 0.00;
+function gameLoop() {
+  if (keysDown.has("ArrowUp")) {
+    speed += 0.02;
+  } else if (keysDown.has("ArrowDown")) {
+    speed -= 0.02;
+  } else {
+    speed *= 0.98;
+  }
+  speed = Math.max(Math.min(speed, 1), -1);
+  //console.log("Speed:", speed);
+  const step = 0.05 * speed;
+  const rotStep = 0.05;
+  let outOfBounds = false;
   let prevX, prevY;
-  const key = event.key;
-  if (key === "ArrowUp") {
-    
     prevX = worldState.lawnMowerPosition.x;
     prevY = worldState.lawnMowerPosition.y;
     worldState.lawnMowerPosition.x += step * Math.cos(worldState.lawnMowerOrientation);
     worldState.lawnMowerPosition.y += step * Math.sin(worldState.lawnMowerOrientation);
-    lawnMowerCtx.strokeStyle = 'black';
-    lawnMowerCtx.lineWidth = 100;
-    lawnMowerCtx.beginPath();
-    lawnMowerCtx.moveTo(
+    if (worldState.lawnMowerPosition.x < -1) worldState.lawnMowerPosition.x = -1, outOfBounds = true;
+    if (worldState.lawnMowerPosition.x > 1) worldState.lawnMowerPosition.x = 1, outOfBounds = true;
+    if (worldState.lawnMowerPosition.y < -1) worldState.lawnMowerPosition.y = -1, outOfBounds = true;
+    if (worldState.lawnMowerPosition.y > 1) worldState.lawnMowerPosition.y = 1, outOfBounds = true;
+    if (outOfBounds) {
+      // prevX = worldState.lawnMowerPosition.x;
+      // prevY = worldState.lawnMowerPosition.y;
+      speed = 0;
+    }
+    lawnCtx.strokeStyle = 'black';
+    lawnCtx.lineWidth = 80;
+    lawnCtx.beginPath();
+    lawnCtx.moveTo(
       (prevX + 0.5) * 512,
       (prevY + 0.5) * 512
     );
-    lawnMowerCtx.lineTo(
+    lawnCtx.lineTo(
       (worldState.lawnMowerPosition.x + 0.5) * 512,
       (worldState.lawnMowerPosition.y + 0.5) * 512
     );
-    lawnMowerCtx.stroke();
+    lawnCtx.stroke();
+  if (keysDown.has("ArrowLeft")) {
+    worldState.lawnMowerOrientation += rotStep;
   }
-  if (key === "ArrowDown") {
-    prevX = worldState.lawnMowerPosition.x;
-    prevY = worldState.lawnMowerPosition.y;
-    worldState.lawnMowerPosition.y += -step * Math.sin(worldState.lawnMowerOrientation);
-    worldState.lawnMowerPosition.x += -step * Math.cos(worldState.lawnMowerOrientation);
-    lawnMowerCtx.strokeStyle = 'black';
-    lawnMowerCtx.lineWidth = 100;
-    lawnMowerCtx.beginPath();
-    lawnMowerCtx.moveTo(
-      (prevX + 0.5) * 512,
-      (prevY + 0.5) * 512
-    );
-    lawnMowerCtx.lineTo(
-      (worldState.lawnMowerPosition.x + 0.5) * 512,
-      (worldState.lawnMowerPosition.y + 0.5) * 512
-    );
-    lawnMowerCtx.stroke();
-  }
-  if (key === "ArrowLeft") {
+  if (keysDown.has("ArrowRight")) {
     worldState.lawnMowerOrientation -= rotStep;
   }
-  if (key === "ArrowRight") {
-    worldState.lawnMowerOrientation += rotStep;
+  lawnSampleCtx.clearRect(0, 0, 16, 16);
+  lawnSampleCtx.drawImage(lawnCanvas, 0, 0, 512, 512, 0, 0, 16, 16);
+  const data = lawnSampleCtx.getImageData(0, 0, 16, 16);
+  let allShort = true;
+  let cumBrightness = 0;
+  //console.log("data.data.length:", data.data.length);
+  for (let i = 0; i < data.data.length; i += 4) {
+    const r = data.data[i];
+    const g = data.data[i + 1];
+    const b = data.data[i + 2];
+    const a = data.data[i + 3];
+    const brightness = (r + g + b) / 3;
+    cumBrightness += brightness;
+    //console.log("brightness:", cumBrightness);
+    if (brightness > 5) {
+      allShort = false;
+    }
+  }
+  cumBrightness /= (data.data.length / 4);
+  //console.log("cumBrightness:", cumBrightness);
+  const elapsedSeconds = Math.floor((Date.now() - startTime) / 1000);
+  const minutes = String(Math.floor(elapsedSeconds / 60)).padStart(2, '0');
+  const seconds = String(elapsedSeconds % 60).padStart(2, '0');
+  //console.log("allShort:", allShort);
+  if (allShort) {
+    document.getElementById("clock").innerText = `Done!. Time: ${minutes}:${seconds} 🎉`;
+  } else {
+    document.getElementById("clock").innerText = `Time: ${minutes}:${seconds}`;
   }
 
   updateDrawing(worldState);
-});
+  requestAnimationFrame(gameLoop);
+}
 
-updateDrawing(worldState);
+gameLoop();
 
 
 
